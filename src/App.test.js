40768f5435ea4318896767ad6d060d43f51051c7
Jason Douglas <jasondouglas@Jasons-MacBook-Pro.local>
2020-02-03 11:14:19 -0800

    Added material UI code

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the page heading', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/things to do/i);
   expect(linkElement).toBeInTheDocument();
